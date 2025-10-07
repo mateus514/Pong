394b6c36840c5933c31ce2273185da50deeb51f8
mateus <145375374+mateus514@users.noreply.github.com>
2025-10-07 14:21:21 -0300

    a
    a

--- Pong!Player2/Assets/Scripts/ClientWithID.cs
+++ Pong!Player2/Assets/Scripts/ClientWithID.cs
@@ -11,14 +11,19 @@ public class UdpClientWithId : MonoBehaviour
     Thread receiveThread;
     IPEndPoint serverEP;
     int myId = -1;
+    Vector2 remotePos = Vector2.zero;
 
+    public GameObject remoteCube;
     public GameObject localCube;
+    
+   
+
     public float moveSpeed = 5f; // mesma ideia do P1
 
     void Start()
     {
         client = new UdpClient();
-        serverEP = new IPEndPoint(IPAddress.Parse("10.57.1.148"), 5001);
+        serverEP = new IPEndPoint(IPAddress.Parse("10.57.1.81"), 5001);
         client.Connect(serverEP);
 
         // Thread para ouvir respostas do servidor
@@ -40,6 +45,7 @@ public class UdpClientWithId : MonoBehaviour
         {
             localCube.transform.Translate(Vector2.up * Time.deltaTime * moveSpeed);
         }
+
         if (isPressingDown)
         {
             localCube.transform.Translate(Vector2.down * Time.deltaTime * moveSpeed);
@@ -52,6 +58,18 @@ public class UdpClientWithId : MonoBehaviour
 
         byte[] data = Encoding.UTF8.GetBytes(msg);
         client.Send(data, data.Length);
+        
+        // Atualiza posição do outro jogador
+
+        remoteCube.transform.position = Vector3.Lerp(
+
+            remoteCube.transform.position, 
+            
+            remotePos,
+
+            Time.deltaTime* 10f
+
+        );
     }
 
     void ReceiveData()
@@ -68,8 +86,33 @@ public class UdpClientWithId : MonoBehaviour
                 myId = int.Parse(msg.Substring(7));
                 Debug.Log("[Cliente] Recebi ID = " + myId);
             }
+            else if (msg.StartsWith("POS:"))
+            {
+
+                string[] parts = msg.Substring(4).Split(';');
+
+                if (parts.Length == 3)
+                {
+
+                    int id = int.Parse(parts[0]);
+
+                    if (id != myId)
+                    {
+
+                        float x = float.Parse(parts[1],
+                            CultureInfo.InvariantCulture);
+
+                        float y = float.Parse(parts[2],
+                            CultureInfo.InvariantCulture);
+
+                        remotePos = new Vector3(x, y, 0);
+                    }
+                }
+            }
         }
     }
+
+
 
     void OnApplicationQuit()
     {
